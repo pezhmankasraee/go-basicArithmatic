d788e8ff9ede1c5d566b929f5d5c15c27b4e9c9a
Pezhman Kasraee <pezhmankasraee@gmail.com>
2019-12-19 11:28:27 +0100

    improve the data type from int to float

--- basic.go
+++ basic.go
@@ -2,27 +2,27 @@ package basicoperation
 
 import "errors"
 
-// Add returns the summation of two integers
-func Add(a, b int) int {
+// Add returns the summation of two float64
+func Add(a, b float64) float64 {
 	return a + b
 }
 
-// Sub returns the subtraction of two integers
-func Sub(a, b int) int {
+// Sub returns the subtraction of two float64
+func Sub(a, b float64) float64 {
 	return a - b
 }
 
-// Mul returns the multiplication of two integers
-func Mul(a, b int) int {
+// Mul returns the multiplication of two float64
+func Mul(a, b float64) float64 {
 	return a * b
 }
 
-// Div returns the division of two integers
-func Div(a, b int) (int, error) {
+// Div returns the division of two float64
+func Div(a, b float64) (float64, error) {
 	if a == 0 && b == 0 {
 		return 0, errors.New("ERROR: 0 / 0 is not defined")
 	} else if b == 0 {
-		return 0, errors.New("ERROR: integer / 0 is Infinit")
+		return 0, errors.New("ERROR: float64 / 0 is Infinit")
 	}
 	return a / b, nil
 }
--- basic_test.go
+++ basic_test.go
@@ -4,42 +4,42 @@ import "testing"
 
 // TestAdd tests the function Add
 func TestAdd(t *testing.T) {
-	addAnswer := Add(10, 20)
+	addAnswer := Add(10.0, 20.0)
 	if addAnswer != 30 {
-		t.Errorf(" [ Error ] : Expected 30, Actual %d", addAnswer)
+		t.Errorf(" [ Error ] : Expected 30, Actual %f", addAnswer)
 	}
 }
 
 // TestSub tests the function Sub
 func TestSub(t *testing.T) {
-	subAnswer := Sub(20, 20)
+	subAnswer := Sub(20.0, 20.0)
 	if subAnswer != 0 {
-		t.Errorf(" [ Error ] : Expected 0, Actual %d", subAnswer)
+		t.Errorf(" [ Error ] : Expected 0, Actual %f", subAnswer)
 	}
 }
 
 // TestMul tests the function Mul
 func TestMul(t *testing.T) {
-	mulAnswer := Mul(20, 20)
+	mulAnswer := Mul(20.0, 20.0)
 	if mulAnswer != 400 {
-		t.Errorf(" [ Error ] : Expected 400, Actual %d", mulAnswer)
+		t.Errorf(" [ Error ] : Expected 400, Actual %f", mulAnswer)
 	}
 }
 
 // TestDiv tests the function Div
 func TestDiv(t *testing.T) {
-	divAnswer, _ := Div(20, 20)
+	divAnswer, _ := Div(20.0, 20.0)
 	if divAnswer != 1 {
-		t.Errorf(" [ Error ] : Expected 1, Actual %d", divAnswer)
+		t.Errorf(" [ Error ] : Expected 1, Actual %f", divAnswer)
 	}
 
-	divA, actualErr := Div(10, 0)
-	expectedErr := "ERROR: integer / 0 is Infinit"
+	divA, actualErr := Div(10.0, 0.0)
+	expectedErr := "ERROR: float64 / 0 is Infinit"
 	if divA != 0 || actualErr.Error() != expectedErr {
 		t.Errorf("Expected: %s\nActual Error Message: %s", expectedErr, actualErr)
 	}
 
-	divA, actualErr = Div(0, 0)
+	divA, actualErr = Div(0.0, 0.0)
 	expectedErr = "ERROR: 0 / 0 is not defined"
 	if divA != 0 || actualErr.Error() != expectedErr {
 		t.Errorf("Expected: %s\nActual Error Message: %s", expectedErr, actualErr)
